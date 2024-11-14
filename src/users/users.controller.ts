2c7e65a781ed527659e457cd069d7de9ee7a0952
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-15 01:22:45 +0700

    feat: add sisanya user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,26 +18,46 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    try {
+      return this.usersService.create(createUserDto);
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    try {
+      return this.usersService.findAll();
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    try {
+      return this.usersService.findOne(id);
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    try {
+      return this.usersService.update(id, updateUserDto);
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    try {
+      return this.usersService.remove(id);
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Firestore } from '@google-cloud/firestore';
@@ -9,36 +13,97 @@ export class UsersService {
   private collection = new Firestore().collection('users');
 
   async create(createUserDto: CreateUserDto) {
-    try {
-      const id = v4();
-      await this.collection.doc(id).set({
-        id: id,
-        name: createUserDto.name,
-        email: createUserDto.email,
-        password: createUserDto.password,
-        created_at: new Date().toISOString(),
-        updated_at: new Date().toISOString(),
-      });
-      return { status: 'success', message: 'User created successfully' };
-    } catch (error) {
-      console.error('Error creating user:', error);
+    const id = v4();
+    await this.collection.doc(id).set({
+      id: id,
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: createUserDto.password,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
+    return {
+      status: 'success',
+      message: 'User created successfully',
+      data: { id: id, name: createUserDto.name, email: createUserDto.email },
+    };
+  }
+
+  async findAll() {
+    const snapshot = await this.collection.get();
+    if (!snapshot) {
       throw new InternalServerErrorException();
     }
+
+    const data = [];
+    snapshot.forEach((doc) => {
+      data.push({
+        id: doc.id,
+        name: doc.data().name,
+        email: doc.data().email,
+        created_at: doc.data().created_at,
+        updated_at: doc.data().updated_at,
+      });
+    });
+    return {
+      status: 'success',
+      message: 'All user fetched successfully',
+      data: data,
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findOne(id: string) {
+    const snapshot = await this.collection.doc(id).get();
+    if (!snapshot.exists) {
+      throw new InternalServerErrorException();
+    }
+    const data = {
+      id: snapshot.id,
+      name: snapshot.data().name,
+      email: snapshot.data().email,
+      created_at: snapshot.data().created_at,
+      update_at: snapshot.data().updated_at,
+    };
+    return {
+      status: 'success',
+      message: 'User fetched successfully',
+      data: data,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const userDoc = this.collection.doc(id);
+    const docSnapshot = await userDoc.get();
+    if (!docSnapshot.exists) {
+      throw new NotFoundException(`User with ID #${id} not found`);
+    }
+
+    // @typescript-eslint/ban-ts-comment
+    await userDoc.update({
+      name: updateUserDto.name,
+      email: updateUserDto.email,
+      password: updateUserDto.password,
+      updated_at: new Date().toISOString(),
+    });
+    return {
+      status: 'success',
+      message: `User with ID #${id} updated successfully`,
+      data: updateUserDto,
+    };
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  async remove(id: string) {
+    const userDoc = this.collection.doc(id);
+    const docSnapshot = await userDoc.get();
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    if (!docSnapshot.exists) {
+      throw new NotFoundException(`User with ID #${id} not found`);
+    }
+
+    await userDoc.delete();
+    return {
+      status: 'success',
+      message: `User with ID #${id} removed successfully`,
+    };
   }
 }
