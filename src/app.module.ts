65a81f5120e6dd427aef8e16ebf30883dd9fe96d
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-15 01:39:11 +0700

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { MlOcrModule } from './ml-ocr/ml-ocr.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TransactionsModule, MlOcrModule],
+  imports: [UsersModule, TransactionsModule, MlOcrModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
