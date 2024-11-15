95d4f44904a82c57be1b5287ba831c617c69c8af
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-15 21:46:46 +0700

    feat: add dummy ocr result

--- src/ml-ocr/ml-ocr.controller.ts
+++ src/ml-ocr/ml-ocr.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  InternalServerErrorException,
+  Post,
+  UploadedFile,
+  // UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MlOcrService } from './ml-ocr.service';
+// import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('predict')
-export class MlOcrController {}
+export class MlOcrController {
+  constructor(private readonly mlOcrService: MlOcrService) {}
+
+  @Post()
+  // @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    try {
+      return this.mlOcrService.uploadFile(file);
+    } catch (e) {
+      throw new InternalServerErrorException(`Unexpected error: ${e.message}`);
+    }
+  }
+}
--- src/ml-ocr/ml-ocr.service.ts
+++ src/ml-ocr/ml-ocr.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MlOcrService {}
+export class MlOcrService {
+  uploadFile(file: Express.Multer.File) {
+    console.log(file);
+    const result = [];
+    const name = ['ayam geprek', 'nasi goreng', 'mie ayam', 'jus jeruk'];
+    const category = ['Makan', 'Belanja', 'Parkir', 'Listrik', 'Laundry'];
+    const type = ['CR', 'DB'];
+    for (let i = 0; i <= Math.floor(Math.random() * 7) + 1; i++) {
+      result.push({
+        amount: Math.random() * 100000,
+        type: type[i % 2],
+        category: category[Math.floor(Math.random() * 4)],
+        name: name[Math.floor(Math.random() * 5)],
+        currency: 'IDR',
+        date: new Date().toISOString(),
+      });
+    }
+    return {
+      status: 'success',
+      message: 'Berhasil melakukan ocr',
+      data: result,
+    };
+  }
+}
