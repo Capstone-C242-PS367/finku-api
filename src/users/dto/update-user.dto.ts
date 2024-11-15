db9628ac49650ecf0c7333c953cbf85512860e0d
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-16 02:06:15 +0700

    feat: add validation in user dto and fix return status

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty({ message: 'user_id is required' })
   name: string;
-  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required.' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long.' })
+  @MaxLength(20, { message: 'Password must not exceed 20 characters.' })
   password: string;
 }
