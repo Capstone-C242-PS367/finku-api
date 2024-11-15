09c72ed596a508e214199cc5893d37d7a5920082
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-16 02:06:53 +0700

    feat: add comment

--- src/ml-ocr/ml-ocr.service.ts
+++ src/ml-ocr/ml-ocr.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@nestjs/common';
 export class MlOcrService {
   uploadFile(file: Express.Multer.File) {
     console.log(file);
+
+    //TODO: INI NANTI DIHAPUS DIGANTI SAMA HASIL OCR BENERAN
+    //dummy doang buat ngetes di md
     const result = [];
     const name = ['ayam geprek', 'nasi goreng', 'mie ayam', 'jus jeruk'];
     const category = ['Makan', 'Belanja', 'Parkir', 'Listrik', 'Laundry'];
@@ -18,6 +21,8 @@ export class MlOcrService {
         date: new Date().toISOString(),
       });
     }
+    ////////////////////////////////////////
+
     return {
       status: 'success',
       message: 'Berhasil melakukan ocr',
