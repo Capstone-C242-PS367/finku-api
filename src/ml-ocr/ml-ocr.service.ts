c19dde6303bf1a2c94bfd08d878fdadc56e4f7c6
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-11-16 19:36:17 +0700

    test: test ci/cd cloud build

--- src/ml-ocr/ml-ocr.service.ts
+++ src/ml-ocr/ml-ocr.service.ts
@@ -8,14 +8,20 @@ export class MlOcrService {
     //TODO: INI NANTI DIHAPUS DIGANTI SAMA HASIL OCR BENERAN
     //dummy doang buat ngetes di md
     const result = [];
-    const name = ['ayam geprek', 'nasi goreng', 'mie ayam', 'jus jeruk'];
+    const name = [
+      'ayam geprek',
+      'nasi goreng',
+      'mie ayam',
+      'jus jeruk',
+      'roti bakar',
+    ];
     const category = ['Makan', 'Belanja', 'Parkir', 'Listrik', 'Laundry'];
     const type = ['CR', 'DB'];
     for (let i = 0; i <= Math.floor(Math.random() * 7) + 1; i++) {
       result.push({
         amount: Math.floor(Math.random() * 100000),
         type: type[i % 2],
-        category: category[Math.floor(Math.random() * 4)],
+        category: category[Math.floor(Math.random() * 5)],
         name: name[Math.floor(Math.random() * 5)],
         currency: 'IDR',
         date: new Date().toISOString(),
